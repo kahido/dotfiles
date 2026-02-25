f97e230f4e1b292253444e4486621c118f9a9bf8
kahido <66074309+kahido@users.noreply.github.com>
2026-02-25 22:41:59 +0100

    Reconfigure blink.cmp

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,86 +1,43 @@
 return {
-  {
-    'saghen/blink.cmp',
-    version = '1.*',
-    enabled = true,
-    dependencies = {
-      "fang2hou/blink-copilot"
+  'saghen/blink.cmp',
+  dependencies = { "fang2hou/blink-copilot" },
+
+  version = '1.*',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = "default",
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<C-y>"] = { "select_and_accept" },
     },
-    opts = function(_, opts)
-      opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
-        default = { "lsp", "snippets", "buffer", "copilot" },
-        providers = {
-          lsp = {
-            name = "lsp",
-            enabled = true,
-            module = "blink.cmp.sources.lsp",
-            score_offset = 90,
-          },
-          buffer = {
-            name = "buffer",
-            enabled = true,
-            max_items = 3,
-            module = "blink.cmp.sources.buffer",
-            min_keyword_length = 2,
-            score_offset = 95,
-          },
-          snippets = {
-            name = "snippets",
-            enabled = true,
-            max_items = 15,
-            min_keyword_length = 2,
-            module = "blink.cmp.sources.snippets",
-            score_offset = 85, -- the higher the number, the higher the priority
-          },
-          copilot = {
-            name = "copilot",
-            module = "blink-copilot",
-            score_offset = 100,
-            async = true,
-          },
-        },
-      })
 
-      opts.cmdline = {
-        enabled = true,
-      }
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono'
+    },
 
-      opts.completion = {
-        -- menu = {
-        --   border = "single",
-        -- },
-        documentation = {
-          auto_show = true,
-          -- window = {
-          --   border = "single",
-          -- },
-        },
-      }
+    completion = {
+      documentation = {
+        auto_show = true,
+      },
+    },
 
-      opts.appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = 'mono'
-      }
+    cmdline = {
+      enabled = true,
+    },
 
-      opts.keymap = {
-        preset = "default",
-        ["<Tab>"] = { "snippet_forward", "fallback" },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-        -- ["<Up>"] = { "select_prev", "fallback" },
-        -- ["<Down>"] = { "select_next", "fallback" },
-        ["<C-k>"] = { "select_prev", "fallback" },
-        ["<C-j>"] = { "select_next", "fallback" },
-
-        -- ["<S-k>"] = { "scroll_documentation_up", "fallback" },
-        -- ["<S-j>"] = { "scroll_documentation_down", "fallback" },
-
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-e>"] = { "hide", "fallback" },
-        ["<C-y>"] = { "select_and_accept" },
-      }
-
-      return opts
-    end,
-  }
+    sources = {
+      default = { 'buffer' } -- { 'lsp', 'snippets', 'buffer', 'copilot' },
+    }
+  },
+  opts_extend = {}
 }
