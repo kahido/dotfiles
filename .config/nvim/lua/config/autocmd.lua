fbfab2badc97311648ca8232a23e70deeac8db72
kahido <66074309+kahido@users.noreply.github.com>
2026-03-16 21:51:06 +0100

    neovim: disable Lsp Highlight Groups

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -15,6 +15,16 @@ vim.api.nvim_create_autocmd({"BufNewFile", "BufReadPost"}, {
   ]]
 })
 
+vim.api.nvim_create_autocmd({"ColorScheme"}, {
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, '@lsp.type.function', {})
+    for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+      vim.api.nvim_set_hl(0, group, {})
+    end
+  end
+})
+
 vim.api.nvim_set_hl(0, 'TrailingWhitespace', { bg='LightRed' })
 -- vim.api.nvim_set_hl(0, 'TrailingWhitespace', { bg='#d75f5f' })
 
